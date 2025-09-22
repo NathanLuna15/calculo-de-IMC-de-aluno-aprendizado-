95d8c5b0cfd26045b88fd2c8df80c0aa42e70f16
NATHAN PROENÇA DE LUNA <25205657@cfp127aluno.local>
2025-09-22 16:57:45 -0300

    inicializando atividade

--- src/br/senai/sp/jandira/ds1t/imc_academia/Main.java
+++ src/br/senai/sp/jandira/ds1t/imc_academia/Main.java
@@ -1,3 +1,5 @@
+package br.senai.sp.jandira.ds1t.imc_academia;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
--- src/br/senai/sp/jandira/ds1t/imc_academia/model/imcAluno.java
+++ src/br/senai/sp/jandira/ds1t/imc_academia/model/imcAluno.java
@@ -1,4 +1,17 @@
 package br.senai.sp.jandira.ds1t.imc_academia.model;
 
+import java.util.Scanner;
+
 public class imcAluno {
+
+    String nome;
+    double altura;
+    double peso;
+
+    public void ObiterIMC(){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("qual é ");
+    }
+
 }
